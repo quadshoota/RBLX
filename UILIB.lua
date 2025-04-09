ddb99a2f8eb8c2fd1849e663839570aca0f72880
quadshoota <95818124+quadshoota@users.noreply.github.com>
2025-04-09 19:20:01 +0200

    Update UILIB.lua

--- UILIB.lua
+++ UILIB.lua
@@ -866,7 +866,7 @@ local Features =
                  --   else
                   --      queue(queueCode .. "loadstring(game:HttpGet('https://raw.githubusercontent.com/Catto-YFCN/Lunor_Dependencies/refs/heads/main/Games'))()")
 
-                        loadstring(game:GetHttp("https://raw.githubusercontent.com/quadshoota/RBLX/refs/heads/main/UILIB.lua"))()
+                        loadstring(game:HttpGet("https://raw.githubusercontent.com/quadshoota/RBLX/refs/heads/main/UILIB.lua"))()
                     --end
                 end
             end
