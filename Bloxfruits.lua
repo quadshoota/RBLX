89490e64e1133ada72d8f5c5d84359e9b1a52580
quadshoota <95818124+quadshoota@users.noreply.github.com>
2025-04-14 15:31:42 +0200

    Update and rename Bloxfruits to Bloxfruits.lua

---
+++ Bloxfruits.lua
@@ -0,0 +1,28 @@
+local Codes =
+{
+    "fudd10",
+    "fudd10_V2",
+    "Chandler",
+    "BIGNEWS",
+    "KITT_RESET",
+    "Sub2UncleKizaru",
+    "SUB2GAMERROBOT_RESET1",
+    "Sub2Fer999",
+    "Enyu_is_Pro",
+    "JCWK",
+    "StarcodeHEO",
+    "MagicBUS",
+    "KittGaming",
+    "Sub2CaptainMaui",
+    "Sub2OfficialNoobie",
+    "TheGreatAce",
+    "Sub2NoobMaster123",
+    "Sub2Daigrock",
+    "Axiore",
+    "StrawHatMaine",
+    "TantaiGaming",
+    "Bluxxy",
+    "SUB2GAMERROBOT_EXP1"
+}
+
+return Codes
