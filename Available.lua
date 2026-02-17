34697a8e60f45afeae14c220afd3d8c1852085cd
quadshoota <95818124+quadshoota@users.noreply.github.com>
2026-02-17 19:14:01 +0100

    Update Available.lua

--- Available.lua
+++ Available.lua
@@ -1,5 +1,5 @@
 local Available = {
-"Lunacy", "browser", "cloudfile", "options", "user", "chart", "role", "calender"
+"Lunacy", "browser", "cloudfile", "options", "user", "chart", "role", "calender", "discord"
 }
  
 return Available
